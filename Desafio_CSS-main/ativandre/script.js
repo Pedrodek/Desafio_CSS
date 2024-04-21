4a28082824412ce0e92a7361affcd9ef32ef6cbe
Pedro <110565971+Pedrodek@users.noreply.github.com>
2024-04-21 14:06:01 -0300

    Add files via upload

---
+++ Desafio_CSS-main/ativandre/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    const fieldmenu = document.querySelector('.fieldset-menu')
+    const menu = document.querySelector('.menu') 
+    const fieldmenuSim = document.querySelector('.fieldset-menu-sim')
+    const sim = document.querySelector('.sim')
+    const nao = document.querySelector('.nao')
+
+    sim.addEventListener('click', ()=> {
+        menu.classList.add('hidden');
+        fieldmenu.classList.add('hidden');
+        fieldmenuSim.classList.remove('hidden');
+
+    })
+
+
+
+
+
+});
