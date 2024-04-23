2acc2ce262b99a12da69209ce3fdd634ec32f1ea
Pedro <110565971+Pedrodek@users.noreply.github.com>
2024-04-22 22:34:50 -0300

    Add files via upload

--- ativandre/script.js
+++ ativandre/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-
+    const body = document.querySelector('body')
     const fieldmenu = document.querySelector('.fieldset-menu')
     const menu = document.querySelector('.menu') 
     const fieldmenuSim = document.querySelector('.fieldset-menu-sim')
@@ -8,14 +8,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const nao = document.querySelector('.nao')
 
     sim.addEventListener('click', ()=> {
+        body.classList.add('active');
         menu.classList.add('hidden');
         fieldmenu.classList.add('hidden');
         fieldmenuSim.classList.remove('hidden');
 
+
     })
-
-
-
 
 
 });
