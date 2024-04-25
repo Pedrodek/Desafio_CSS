a36626073c299638967067b666393a17292ad044
Pedro <110565971+Pedrodek@users.noreply.github.com>
2024-04-25 14:32:45 -0300

    Add files via upload

--- ativandre/fun/script.js
+++ ativandre/fun/script.js
@@ -4,21 +4,32 @@ document.addEventListener('DOMContentLoaded', () => {
     const orbe = document.querySelector('.orbe')
     const quad = document.querySelector('.quadrado-spin')
     const h1 = document.querySelector('.h1secret')
+    let timeoutId; 
 
-    orbe.addEventListener('mousedown', ()=> {
-        setTimeout(function(){
-            body.classList.add('backwhite');                                                
-            orbe.classList.add('hidden')
-            quad.classList.add('hidden')
-            h1.classList.add('backwhite')
+    orbe.addEventListener('mousedown', () => {
+        timeoutId = setTimeout(function() {
+            body.classList.add('backwhite');
+            orbe.classList.add('hidden');
+            quad.classList.add('hidden');
+            h1.classList.add('backwhite');
         }, 9992);
-    })
+    });
+
+    orbe.addEventListener('mouseup', () => {
+        clearTimeout(timeoutId);
+    });
+
     
 });
 
 //amei isso =3
+
+function onYouTubeIframeAPIReady() {
+    carregarPlayer();
+}
+
 function carregarPlayer() {
-    var videoId = 'https://www.youtube.com/watch?v=9lX-hVpvN3E';
+    var videoId = extrairVideoId('https://www.youtube.com/watch?v=9lX-hVpvN3E');
 
     var playerConfig = {
         height: '0',
@@ -45,9 +56,7 @@ function onPlayerReady(event) {
     });
 };
 
-window.onload = function() {
-    var tag = document.createElement('script');
-    tag.src = 'https://www.youtube.com/iframe_api';
-    var primeiroScriptTag = document.getElementsByTagName('script')[0];
-    primeiroScriptTag.parentNode.insertBefore(tag, primeiroScriptTag);
-};
+function extrairVideoId(url) {
+    var match = url.match(/[?&]v=([^&]+)/);
+    return match ? match[1] : null;
+}
