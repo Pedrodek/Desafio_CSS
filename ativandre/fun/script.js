ee59771530907d33d26a27dcbe734b500922d32b
Pedro <110565971+Pedrodek@users.noreply.github.com>
2024-04-25 15:28:29 -0300

    Add files via upload

--- ativandre/fun/script.js
+++ ativandre/fun/script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const quad = document.querySelector('.quadrado-spin')
     const h1 = document.querySelector('.h1secret')
     let timeoutId; 
-
+    
     orbe.addEventListener('mousedown', () => {
         timeoutId = setTimeout(function() {
             body.classList.add('backwhite');
@@ -19,44 +19,21 @@ document.addEventListener('DOMContentLoaded', () => {
         clearTimeout(timeoutId);
     });
 
+    let cat = document.getElementById('cat');
+
+    quad.addEventListener('mouseover', () => {
+        cat.play();
+        cat.volume = 0.15;
+    });
+
+    quad.addEventListener('mouseout', () => {
+        cat.pause();
+        cat.currentTime = 0;
+    });
+
     
 });
 
 //amei isso =3
 
-function onYouTubeIframeAPIReady() {
-    carregarPlayer();
-}
 
-function carregarPlayer() {
-    var videoId = extrairVideoId('https://www.youtube.com/watch?v=9lX-hVpvN3E');
-
-    var playerConfig = {
-        height: '0',
-        width: '0',
-        videoId: videoId,
-        events: {
-            'onReady': onPlayerReady
-        }
-    };
-
-    var player = new YT.Player('player', playerConfig);
-}
-
-function onPlayerReady(event) {
-    var quad = document.getElementById('quad');
-
-    quad.addEventListener('mouseover', function() {
-        event.target.playVideo();
-    });
-
-    quad.addEventListener('mouseout', function() {
-        event.target.pauseVideo(); 
-        event.target.seekTo(0); 
-    });
-};
-
-function extrairVideoId(url) {
-    var match = url.match(/[?&]v=([^&]+)/);
-    return match ? match[1] : null;
-}
